381d7bc3fbd3a5c141e84678c39aab711105fc8d
msaad1311 <msaad1311@users.noreply.github.com>
2022-11-27 04:16:30 -0500

    updated the code to create the features for each image for boys appareal

--- src/data/photo_catcher.py
+++ src/data/photo_catcher.py
@@ -1,25 +1,51 @@
-import numpy as np
 import pandas as pd
-import cv2
-import os
 
-TYPE = 'Apparel'
-GENDER = 'Boys'
-PATH_IMAGE = f'../../data/raw/{TYPE}/{GENDER}/Images/Images_with_product_ids/'
-PATH_FILE = f'../../data/raw/fashion.csv'
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Flatten, AveragePooling2D
+from tensorflow.keras import applications
+from tensorflow.keras.models import Model
 
+TYPE = "Apparel"
+GENDER = "Boys"
+PATH_IMAGE = f"../../data/raw/{TYPE}/{GENDER}/Images/"
+PATH_FILE = r"../../data/raw/fashion.csv"
+IMAGE_WIDTH, IMAGE_HEIGHT = 224, 224
+EPOCHS = 50
+BATCH_SIZE = 1
+RESNET_MODEL = applications.ResNet50(
+    include_top=False,
+    weights="imagenet",
+    input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 3),
+)
+output = RESNET_MODEL.output
+output = AveragePooling2D(pool_size=(7, 7))(RESNET_MODEL.output)
+output = Flatten()(output)
+model = Model(inputs=RESNET_MODEL.input, outputs=output)
 
-temporary_image_name = os.listdir(PATH_IMAGE)[0]
-print(temporary_image_name)
-image = cv2.imread(PATH_IMAGE + temporary_image_name)
-print(image.shape)
-image_dictionary = {int(temporary_image_name.split('.')[0]): image}
-image_table = pd.DataFrame(image_dictionary,columns=['ids','Arrays'])
+FILENAMES = []
 
 fashion_lookup = pd.read_csv(PATH_FILE)
-fashion_lookup = fashion_lookup[fashion_lookup['ProductId']== int(temporary_image_name.split('.')[0])]
-final_fashion_table = pd.merge(left = fashion_lookup,
-right = image_table, left_on = ['ProductId'], right_on=['ids'])
+sample_size = len(fashion_lookup[fashion_lookup["Gender"] == GENDER])
 
+# Trying out the feature extraction
+datagen = ImageDataGenerator(
+    rotation_range=45,
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1 / 255,
+)
 
+generator = datagen.flow_from_directory(
+    PATH_IMAGE,
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=BATCH_SIZE,
+    class_mode=None,
+    shuffle=False,
+)
 
+for filename in generator.filenames:
+    FILENAMES.append(filename.split("/")[-1])
+
+image_features = model.predict_generator(
+    generator, sample_size // BATCH_SIZE
+)
